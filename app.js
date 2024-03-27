3af3821739b78d942928620769567d541d310b62
Bharath Simha Reddy <84339476+kothapetaBharathSimhaReddy@users.noreply.github.com>
2024-03-27 16:40:47 -0500

    Bonus

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var ToolsRouter = require('./routes/Tools');
 var gridRouter = require('./routes/grid');
+var pickRouter = require('./routes/pick');
 
 var app = express();
 
@@ -25,6 +26,8 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/Tools', ToolsRouter);
 app.use('/grid', gridRouter);
+app.use('/pick', pickRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
